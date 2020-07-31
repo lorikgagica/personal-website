28b56b2cbdf6aae548e97955abac123ea7102ae1
Lorik Gagica <lorik.lg8@gmail.com>
2020-07-31 21:18:39 +0200

    Removed animejs

---
+++ js/scrypt.js
@@ -0,0 +1,10 @@
+import anime from 'node_modules/animejs/lib/anime.es.js';
+
+
+anime({
+    targets: 'div',
+    translateX: 250,
+    rotate: '1turn',
+    backgroundColor: '#333333',
+    duration: 800
+  });
